3b1a6ba9532f66e01914d6dc796f3d9595ecb835
EthanLavi <89608076+EthanLavi@users.noreply.github.com>
2024-05-27 19:16:08 +0000

    Fake memory pool for testing

---
+++ dcache/test/faux_mempool.h
@@ -0,0 +1,80 @@
+#pragma once
+#include <cstdint>
+#include <cstdlib>
+
+#include <remus/logging/logging.h>
+#include <remus/rdma/rdma.h>
+
+using namespace remus::rdma;
+using namespace std;
+
+class CountingPool {
+    unordered_map<void*, int> allocat; // ptr to size
+    bool locality;
+
+public:
+    CountingPool(bool all_local) : locality(all_local) {}
+
+    template <typename T>
+    rdma_ptr<T> Allocate(int size = 1){
+        int bytes = sizeof(T) * size;
+        T* p = (T*) malloc(bytes);
+        allocat[(void*) p] = bytes;
+        return rdma_ptr<T>(0, p);
+    }
+
+    template <typename T>
+    void Deallocate(rdma_ptr<T> p, int size = 1){
+        int bytes = sizeof(T) * size;
+        void* add = (void*) p.address();
+        REMUS_ASSERT(allocat.find(add) != allocat.end(), "Found double free with ptr {}", p);
+        REMUS_ASSERT(allocat.at(add) == bytes, "Found free with ptr {} with wrong size (actual={} != expected={})", p, allocat.at(add), bytes);
+        allocat.erase(add);
+        free((T*) p.address());
+
+    }
+
+    template <typename T>
+    rdma_ptr<T> Read(rdma_ptr<T> p, rdma_ptr<T> prealloc = nullptr){
+        REMUS_ASSERT(prealloc == nullptr, "precondition, missing feature");
+        return ExtendedRead(p, 1);
+    }
+
+    template <typename T>
+    rdma_ptr<T> ExtendedRead(rdma_ptr<T> p, int size, rdma_ptr<T> prealloc = nullptr){
+        REMUS_ASSERT(prealloc == nullptr, "precondition, missing feature");
+        rdma_ptr<T> p_new = Allocate<T>(size);
+        *p_new = *p;
+        return p_new;
+    }
+
+    template <typename T>
+    void Write(rdma_ptr<T>& ptr, const T& val, rdma_ptr<T> prealloc = nullptr, internal::RDMAWriteBehavior write_behavior = internal::RDMAWriteWithAck){
+        REMUS_ASSERT(prealloc == nullptr, "precondition, missing feature");
+        *ptr = val;
+    }
+
+    template <typename T>
+    T CompareAndSwap(rdma_ptr<T> ptr, uint64_t expected, uint64_t swap) {
+        uint64_t prev = *ptr;
+        if (prev == expected){
+            *ptr = swap;
+        }
+        return prev;
+    }
+
+    template <typename T>
+    bool is_local(rdma_ptr<T> p){
+        return locality;
+    }
+
+    bool HasNoLeaks(){
+        return allocat.size() == 0;
+    }
+
+    void debug(){
+        for(auto it = allocat.begin(); it != allocat.end(); it++){
+            REMUS_WARN("{} was not freed", it->first);
+        }
+    }
+};
