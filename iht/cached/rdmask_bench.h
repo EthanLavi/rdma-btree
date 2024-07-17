d4695588021d3dfc28eee5491adb15911a06240c
EthanLavi <89608076+EthanLavi@users.noreply.github.com>
2024-07-17 17:41:06 +0000

    rdmask with helper thread

--- iht/cached/rdmask_bench.h
+++ iht/cached/rdmask_bench.h
@@ -14,30 +14,38 @@
 
 using namespace remus::util;
 
-typedef RdmaSkipList<int, int, 7, INT_MIN> RDMASK;
-
-template<> inline thread_local CacheMetrics RemoteCacheImpl<CountingPool>::metrics = CacheMetrics();
-template<> inline thread_local CountingPool* RemoteCacheImpl<CountingPool>::pool = nullptr;
+typedef RdmaSkipList<int, 7, INT_MIN, ULONG_MAX, ULONG_MAX - 1> RDMASK;
 
 inline void rdmask_run(BenchmarkParams& params, rdma_capability* capability, RemoteCache* cache, Peer& host, Peer& self){
     CountingPool* pool = new CountingPool(false);
     RemoteCacheImpl<CountingPool>* cach = new RemoteCacheImpl<CountingPool>(pool);
     RemoteCacheImpl<CountingPool>::pool = pool; // set pool to other pool so we acccept our own cacheline
-    
-    RDMASK sk = RDMASK(self, CacheDepth::RootOnly, cach, pool);
-    sk.InitAsFirst(pool);
-    REMUS_INFO("DONE INIT");
-    REMUS_INFO("Insert(0, 0) = {}", sk.insert(pool, 0, 0).value_or(-1));
-    REMUS_INFO("Insert(1, 1) = {}", sk.insert(pool, 1, 1).value_or(-1));
-    REMUS_INFO("Insert(2, 2) = {}", sk.insert(pool, 2, 2).value_or(-1));
-    REMUS_INFO("Insert(5, 5) = {}", sk.insert(pool, 5, 5).value_or(-1));
-    REMUS_INFO("Insert(4, 4) = {}", sk.insert(pool, 4, 4).value_or(-1));
-    REMUS_INFO("Insert(3, 3) = {}", sk.insert(pool, 3, 3).value_or(-1));
-    sk.debug();
-    for(int i = 0; i <= 5; i++)
-        REMUS_ASSERT(sk.contains(pool, i).value_or(-1) == i, "Contains({}) is valid", i);
 
-    sk.remove(pool, 2);
+    RDMASK sk = RDMASK(self, CacheDepth::RootOnly, cach, pool, 1, {self});
+    sk.InitAsFirst(pool);
+    sk.RegisterThread();
+    std::atomic<bool> do_cont;
+    do_cont.store(true);
+    std::thread t = std::thread([&](){
+        RemoteCacheImpl<CountingPool>::pool = pool; // initialize the thread_local
+        sk.helper_thread(&do_cont, pool);
+    });
+    REMUS_INFO("DONE INIT");
+    sk.populate(pool, 64, 0, 100, std::function([=](int x){ return x; }));
+    // REMUS_INFO("Remove(6) = {}", (int64_t) sk.remove(pool, 6).value_or(-1));
+    // sk.debug();
+    // REMUS_INFO("Insert(6) = {}", (int64_t) sk.insert(pool, 6, 6).value_or(-1));
+    // sk.debug();
+
+    int counter = 0;
+    for(int i = 0; i < 10000; i++){
+        if (sk.contains(pool, i).has_value()) counter++;
+    }
+    REMUS_INFO("matched_keys = {}", counter);
+    REMUS_INFO("Count() = {}", sk.count(pool));
+
+    do_cont.store(false);
+    t.join();
     sk.debug();
 
     cach->free_all_tmp_objects();
