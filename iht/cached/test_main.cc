4ca739af38d29f1d4795de07f93f19983ed3666b
EthanLavi <89608076+EthanLavi@users.noreply.github.com>
2024-07-17 17:35:33 +0000

    Args

--- iht/cached/test_main.cc
+++ iht/cached/test_main.cc
@@ -1,9 +1,21 @@
-// #include "btree_bench.h"
+#include "btree_bench.h"
 #include "rdmask_bench.h"
 
-int main(){
+template<> inline thread_local CacheMetrics RemoteCacheImpl<CountingPool>::metrics = CacheMetrics();
+template<> inline thread_local CountingPool* RemoteCacheImpl<CountingPool>::pool = nullptr;
+
+int main(int argc, char* argv[]){
+    if (argc == 1) {
+        REMUS_ERROR("Missing datastructure");
+        return 1;
+    }
+    std::string structure(argv[1]);
     Peer peer = Peer();
     BenchmarkParams params = BenchmarkParams();
-    // btree_run(params, nullptr, nullptr, peer, peer);
-    rdmask_run(params, nullptr, nullptr, peer, peer);
+    if (structure == "btree" || structure == "b")
+        btree_run(params, nullptr, nullptr, peer, peer);
+    else if (structure == "rdmask" || structure == "sk")
+        rdmask_run(params, nullptr, nullptr, peer, peer);
+    else
+        REMUS_ERROR("No valid structure");
 }
